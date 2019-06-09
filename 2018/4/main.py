52e1c0f43f95ea6253ee4f5307a19efb407f3bf8
Georgiana Chelu <georgiana.chelu93@gmail.com>
2019-06-09 12:31:48 +0300

    [adventofcode 2018] Day 4, part 1; nice solution; update readme with hints

--- 2018/4/main.py
+++ 2018/4/main.py
@@ -1,132 +1,31 @@
-import re
-from datetime import datetime, time, timedelta
-
-# def get_date(time):
-
-
-def fix_date(time):
-	# strip the [] parentheses
-	date = time[0][1:]
-	hour = time[1][:-1]
-
-	# parse the date
-	dd = [int(dx) for dx in date.split("-")]
-	d = datetime(dd[0], dd[1], dd[2])
-
-
-	# parse the hour and increse the time by 1 day
-	# in case the hour is before midnight
-	hh = hour.split(":")
-	hh = [int(hx) for hx in hh]
-	td = timedelta(days=0, hours=hh[0], minutes=hh[1])
-	if hh[0] != 0:
-		td = timedelta(days=1)
-
-	d = d + td
-
-	return "{:%Y-%m-%d %H:%M}".format(d)
+import re, fileinput, operator
+from collections import defaultdict
 
 def main():
-	dates = {}
-	guards_asleep_time = {}
-
-	with open('input') as file:
-		lines = file.readlines()
-
-	for line in lines:
-		# match everthing inside []
-		tmp = re.findall("\\[.*\\]", line)
-		time = tmp[0].split(" ")
-		time = fix_date(time)
-
-		# match evething afer ]
-		action = re.findall("(?<=\\] ).*$", line)
-
-		date = time.split(" ")[0]
-		hour = time.split(" ")[1]
-
-		action.append(hour)
-		if date in dates:
-			dates[date].append(action)
-		else:
-			dates[date] = [action]
-
-	for key in dates:
-		sleep = 0
-		# print(key, dates[key])
-		for action in dates[key]:
-			# the action. eg falls asleep, wakes up, begins shift
-			act = action[0]
-			# the time of the action. eg: 00:03. keep just the minutes
-			act_time = int(action[1].split(":")[1])
-			# the amount of time the guard sleeps during his shift
-
-			if 'falls' in act:
-				sleep -= act_time
-			if 'wakes' in act:
-				sleep += act_time
-			if 'Guard' in act:
-				guard_nr = re.findall("#[0-9]*", act)[0][1:]
-
-		if guard_nr in guards_asleep_time:
-			old_asleep_time = guards_asleep_time[guard_nr]
-			new_asleep_time = old_asleep_time + sleep
-			guards_asleep_time[guard_nr] = new_asleep_time
-		else:
-			guards_asleep_time[guard_nr] = sleep
-
-	most_asleep_min = 0
-	sleepy_guard = 0
-	for key in guards_asleep_time:
-		if most_asleep_min < guards_asleep_time[key]:
-			most_asleep_min = guards_asleep_time[key]
-			sleepy_guard = key
-
-	print("The sleepyiest guard is ")
-	print(sleepy_guard, most_asleep_min)
+	# each entry has 0 as default value
+	total_time = defaultdict(int)
+	sleeping_time = defaultdict(lambda: defaultdict(int))
 
 
-	tmp = []
-	for key in dates:
-		for action in dates[key]:
-			if sleepy_guard in action[0]:
-				tmp.append(key)
-				continue
+	for line in sorted(fileinput.input()):
 
-	minutes = []
-	intervals = []
+		# parse the minute
+		minute = int(re.search(r':(\d+)', line).group(1))
+		if "#" in line:
+			# parse the guard ID
+			guard = int(re.search(r'#(\d+)', line).group(1))
+		if "falls" in line:
+			m0 = minute
+		if "wakes" in line:
+			m1 = minute
+			for m in range(m0, m1):
+				total_time[guard] += 1
+				sleeping_time[guard][m] += 1
 
-	for t in tmp:
-		minutes = []
-		for action in dates[t]:
-			if "Guard" not in action[0]:
-				hour = action[1]
-				minute = int(hour.split(":")[1])
-				minutes.append(minute)
-
-		minutes = sorted(minutes)
-		print(minutes)
-		while minutes:
-			p1 = minutes[0]
-			p2 = minutes[1]
-			intervals.append([p1,p2])
-			minutes = minutes[2:]
-	print(intervals)
-
-	r, c = 60, 60
-
-	matrix = [[0 for i in range(0,r)] for j in range(0,c)]
-	index = 0
-	for interval in intervals:
-		r_start, r_end = interval
-		while r_start < r_end:
-			matrix[r_start][index] = 1
-			r_start += 1
-		index += 1
-
-	print (matrix[1])
-	sums = [sum(matrix[i]) for i in range(0,r)]
+	# sort by value and get element with the greatest value
+	sleepy_guard, _ = sorted(total_time.items(), key=operator.itemgetter(1))[-1]
+	golden_minute, _ = sorted(sleeping_time[sleepy_guard].items(), key=operator.itemgetter(1))[-1]
+	print (sleepy_guard * golden_minute)
 
 
-	golden_minute = 40
 main()

a375c2714f4bc9e7bac309b89a1d35aa75e318f6
Georgiana Chelu <georgiana.chelu93@gmail.com>
2019-07-28 23:44:35 +0300

    [adventofcode 2018] Day 4 Part 2

--- 2018/4/main.py
+++ 2018/4/main.py
@@ -6,8 +6,9 @@ def main():
 	total_time = defaultdict(int)
 	sleeping_time = defaultdict(lambda: defaultdict(int))
 
+	input_txt = sorted(fileinput.input())
 
-	for line in sorted(fileinput.input()):
+	for line in input_txt:
 
 		# parse the minute
 		minute = int(re.search(r':(\d+)', line).group(1))
@@ -25,7 +26,15 @@ def main():
 	# sort by value and get element with the greatest value
 	sleepy_guard, _ = sorted(total_time.items(), key=operator.itemgetter(1))[-1]
 	golden_minute, _ = sorted(sleeping_time[sleepy_guard].items(), key=operator.itemgetter(1))[-1]
-	print (sleepy_guard * golden_minute)
+	print ("Part 1:", sleepy_guard * golden_minute)
+
+	sleepy_minutes = defaultdict(int)
+	for guard in sleeping_time:
+		sleepy_minutes[guard] = sorted(sleeping_time[guard].items(), key=lambda a: a[1])[-1]
+
+	# determine the values to compare using the lambda function
+	sleepy_guard_2, (sleepy_min, _) = max(sleepy_minutes.items(), key=lambda a: a[1][1])
+	print("Part 2:", sleepy_guard_2 * sleepy_min)
 
 
 main()

a1ae64d16804596d502d41650ae12c5bc15b4792
Georgiana Chelu <georgiana.chelu93@gmail.com>
2021-01-03 22:13:51 +0200

    [2020] Day 8 | Part 2

--- 2020/day_8/day8.py
+++ 2020/day_8/day8.py
@@ -52,5 +52,53 @@ def part1():
             i += 1
         
 
+def part2():
+    lines = get_input()
+    instructions = process_input(lines)
 
-print("The result for Part1 is: ", part1())
+    n = len(instructions)
+
+    for idx in range(n):
+        instructions = process_input(lines)
+        acc = 0
+        i = 0
+        c_instr, c_arg, c_v = instructions[idx]
+
+        if c_instr == JMP:
+            c_instr = NOP
+        elif c_instr == NOP:
+            c_instr = JMP
+
+        instructions[idx] = c_instr, c_arg, c_v
+
+        while True:
+            instr, arg, visited = instructions[i]
+
+            # instruction was already executed
+            if visited == True:
+                break
+
+            # mark the instruction as executed
+            instructions[i] = (instr, arg, True)
+
+            if instr == JMP:
+                i += arg
+            elif instr == ACC:
+                acc += arg
+                i += 1
+            elif instr == NOP:
+                i += 1
+
+            if i >= n:
+                return acc
+
+        if c_instr == JMP:
+            c_instr = NOP
+        elif c_instr == NOP:
+            c_instr = JMP
+
+        instructions[idx] = c_instr, c_arg, c_v
+
+
+print("The result for Part 1 is: ", part1())
+print("The result for Part 2 is: ", part2())
